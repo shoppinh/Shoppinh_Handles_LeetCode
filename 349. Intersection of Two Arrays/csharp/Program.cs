b5a2a248c16c2e99e78cfbab217418deb81feecf
kien.mac <mactrungkien2000@gmail.com>
2025-01-21 23:29:09 +0700

    21-01-2025

---
+++ 349. Intersection of Two Arrays/csharp/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace KienMac
+{
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            // Using built-in function
+            //return nums1.Intersect(nums2).ToArray();
+
+            // Using HashSet
+            //var set1 = new HashSet<int>(nums1);
+            //var set2 = new HashSet<int>(nums2);
+            //var result = new List<int>();
+            //foreach (var item in set2)
+            //{
+            //    if (set1.Contains(item))
+            //    {
+            //        result.Add(item);
+            //    }
+            //}
+            //return result.ToArray();
+
+            // Using Dictionary
+            var result = new List<int>();
+            var map = new Dictionary<int, int>();
+            foreach (var item in nums1)
+            {
+                map.TryAdd(item, 1);
+            }
+
+            foreach (var item in nums2)
+            {
+                if (map.ContainsKey(item))
+                {
+                    result.Add(item);
+                    map.Remove(item);
+                }
+            }
+            return result.ToArray();
+
+
+
+        }
+
+
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+
+            foreach (var variable in s.Intersection([1, 2, 3], [3, 4, 5]))
+            {
+                Console.WriteLine("Result {0}", variable);
+            }
+        }
+    }
+
+
+}
---
+++ 350. Intersection of Two Arrays II/csharp/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace KienMac
+{
+    public class Solution
+    {
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            //using dictionary time-complex: n + m , space-complex: n + k
+            //var dict1 = new Dictionary<int, int>();
+            //var result = new List<int>();
+
+            //foreach (var num in nums1)
+            //{
+            //    if (!dict1.TryAdd(num, 1))
+            //    {
+            //        dict1[num]++;
+            //    }
+            //}
+
+            //foreach (var num in nums2)
+            //{
+            //    if (!dict1.TryGetValue(num, out var count) || count <= 0) continue;
+            //    result.Add(num);
+            //    dict1[num]--;
+            //}
+
+            //return result.ToArray();
+
+            //sorting time-complex: nlogn + mlogm , space-complex: n + m +k
+            Array.Sort(nums1);
+            Array.Sort(nums2);
+            var result = new List<int>();
+
+            var i = 0;
+            var j = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] == nums2[j])
+                {
+                    result.Add(nums1[i]);
+                    i++;
+                    j++;
+                }
+                else if (nums1[i] < nums2[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return result.ToArray();
+        }
+
+
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var nums1 = new int[] { 1, 2, 2, 1 };
+            var nums2 = new int[] { 2, 2 };
+            var result = solution.Intersect(nums1, nums2);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
