21bd3f22c7fe0b68a1daffdb8c816e3ab022a5d4
kien.mac <mactrungkien2000@gmail.com>
2025-01-27 01:06:57 +0700

    26-01-2025

---
+++ 933. Number of Recent Calls/csharp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace KienMac
+{
+    public class RecentCounter
+    {
+        private List<int> records;
+        private int start;
+        public RecentCounter()
+        {
+            records = new List<int>();
+            start = 0;
+
+        }
+
+        public int Ping(int t)
+        {
+           while (start < records.Count && records[start] < t - 3000)
+            {
+                // Increment the start pointer to remove the unnecessary records
+                start++;
+            }
+            records.Add(t);
+            return records.Count - start;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RecentCounter obj = new RecentCounter();
+            int param_1 = obj.Ping(1);
+            int param_2 = obj.Ping(100);
+            int param_3 = obj.Ping(3001);
+            int param_4 = obj.Ping(3002);
+            //Console.WriteLine(param_);
+        }
+    }
+
+    /**
+     * Your RecentCounter object will be instantiated and called as such:
+     * RecentCounter obj = new RecentCounter();
+     * int param_1 = obj.Ping(t);
+     */
+}
