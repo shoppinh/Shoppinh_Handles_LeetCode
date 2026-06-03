9f2ccf19c983ad13c08d953a879177e2eb1b4466
shoppinh <mactrungkien2000@gmail.com>
2026-06-03 22:31:27 +0700

    Implement Binary Search in JavaScript and Python; remove old project files

---
+++ 704. Binary Search/main.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums)-1
+
+        while (left < right):
+            middle = left + ((right - left) // 2) 
+            if target < nums[middle]:
+                right = middle -1
+            elif target > nums[middle]:
+                left = middle + 1
+            else:
+                return middle
+
+
+        return -1
+        
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.search([-1,0,3,4,9,12], 11))
