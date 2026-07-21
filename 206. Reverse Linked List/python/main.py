fc5f63481a3ff9c3d7f299977ca8f55517f39e61
kien.mac <kien.mac@niteco.se>
2026-07-21 10:33:08 +0700

    Additional approach for reverse linked list problem

--- 206. Reverse Linked List/python/main.py
+++ 206. Reverse Linked List/python/main.py
@@ -4,22 +4,33 @@ class ListNode(object):
         self.val = val
         self.next = next
 class Solution(object):
+    # Approach 1: Recursion
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        def printList(head):
-            while head is not None:
-                print(head.val)
-                head = head.next
         result = None
         while head is not None:
             result = ListNode(head.val,result)
             head = head.next
-        printList(result)
         return result
-    
+
+# Approach 2: Iterative with swap
+    def reverseList2(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        prev = None
+        while curr is not None:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
 if __name__ == '__main__':
+
     s = Solution()
     s.reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
